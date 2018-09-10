3e757d4668328af6b140af59d3c50d67cee68c87
Viet Doan <vtahdn@icloud.com>
2018-09-10 11:17:45 +0700

    Initial Commit

---
+++ UISplitViewController_MasterDetailTemplate_v2Tests/UISplitViewController_MasterDetailTemplate_v2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UISplitViewController_MasterDetailTemplate_v2Tests.swift
+//  UISplitViewController_MasterDetailTemplate_v2Tests
+//
+//  Created by macbook pro  on 9/10/18.
+//  Copyright © 2018 Viet. All rights reserved.
+//
+
+import XCTest
+@testable import UISplitViewController_MasterDetailTemplate_v2
+
+class UISplitViewController_MasterDetailTemplate_v2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
